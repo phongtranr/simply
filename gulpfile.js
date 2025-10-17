b854f5147e7cd31c2811970f50bea3592b127010
Hai Phong Tran <phongtranresearch@gmail.com>
2025-10-17 08:42:38 +0000

    Restore master to snapshot at backup/master/20251017-014435 (fc63940518333d073d8fe700f065ee24c0662d6a)

--- gulpfile.js
+++ gulpfile.js
@@ -176,7 +176,7 @@ function zipper (done) {
       'locales/*.json',
       '*.hbs',
       'partials/**',
-      'podcast/**',
+      'podcast/**',      
       'LICENSE',
       'package.json',
       'README.md',
@@ -188,6 +188,10 @@ function zipper (done) {
     dest('dist')
   ], handleError(done))
 }
+
+
+
+
 // TryGhost Admin
 const dotenv = require('dotenv')
 const path = require('path')
@@ -225,6 +229,7 @@ async function deploy (done) {
     handleError(done)
   }
 }
+
 
 const cssWatcher = () => watch('src/css/**', styles)
 const jsWatcher = () => watch(['src/js/**', '*.js'], scripts)
